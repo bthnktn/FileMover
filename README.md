88f3a18847aed73248e48849ef2b25f43d56990f
Batuhan Keten <bthnktn@gmail.com>
2021-07-08 19:32:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Lint the code
+
+```bash
+yarn lint
+```
+
+## Running tests
+
+```bash
+yarn test
+```
+
+> NOTE: If you believe that some scenarios are missing in the tests, don't hesitate to add them to suite.
+
+## Format the code
+
+```bash
+yarn format
+``` 
---
+++ src/move.ts
@@ -0,0 +1,38 @@
+interface File {
+  id: string;
+  name: string;
+}
+
+interface Folder extends File {
+  files: File[];
+}
+
+type List = Folder[];
+
+export default function move(list: List, source: string, destination: string): List {
+  let sourceFile: File;
+
+  return list.map((folder: Folder) => {
+    if (folder.id === source) {
+      throw new Error('You cannot move a folder');
+    }
+
+    const fileAsDestination = folder.files.some((file: File) => file.id === destination);
+
+    if (fileAsDestination) {
+      throw new Error('You cannot specify a file as the destination');
+    }
+
+    const fileInFolder = folder.files.find((file: File) => file.id === source);
+    const updatedFolder = folder;
+
+    if (fileInFolder) {
+      sourceFile = fileInFolder;
+      updatedFolder.files = folder.files.filter((file: File) => file.id !== fileInFolder.id);
+    } else if (sourceFile && folder.id === destination) {
+      updatedFolder.files.push(sourceFile);
+    }
+
+    return updatedFolder;
+  });
+}
